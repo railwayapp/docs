7e27fa520882d27c512d0dbe1af0569ad7eb810f
Jake Runzer <jakerunzer@gmail.com>
2021-01-15 15:01:52 -0700

    add fathom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "../styles/theme";
 import { OverlayProvider } from "react-aria";
 import "../styles/fonts.css";
+import { useFathom } from "../hooks/useFathom";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useFathom("JPJBANQN", "docs.railway.app");
+
   return (
     <ThemeProvider>
       <OverlayProvider>
