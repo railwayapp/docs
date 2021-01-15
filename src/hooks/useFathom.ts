bc68247bf8b0180cfd0c59e4a5390ecf14ccbb5c
Jake Runzer <jakerunzer@gmail.com>
2021-01-15 15:29:00 -0700

    use custom fathom tracking url

--- src/hooks/useFathom.ts
+++ src/hooks/useFathom.ts
@@ -8,7 +8,7 @@ export const useFathom = (trackingCode: string, siteUrl: string) => {
   useEffect(() => {
     // Initialize Fathom when the app loads
     Fathom.load(trackingCode, {
-      url: "https://cdn.usefathom.com/script.js",
+      url: "https://hare.railway.app/script.js",
       includedDomains: [siteUrl],
     });
 
