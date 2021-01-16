dc455eb0eee11a7037a45c48e9335a8f541b90a3
Jake Runzer <jakerunzer@gmail.com>
2021-01-16 10:56:10 -0700

    remove uunused component

--- src/components/Button.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ButtonHTMLAttributes } from "react";
-
-export const Button: React.FC<
-  ButtonHTMLAttributes<HTMLButtonElement>
-> = props => <button className="btn-primary" {...props} />;
