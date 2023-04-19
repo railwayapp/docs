84e121a333a7ca0bf55ff0feb3654d4029b91d17
Ray Chen <ray@raychen.io>
2023-04-19 16:47:18 +0800

    fix(search): Pin docs-scraper image version and tweak sitemap URL gen (#237)
    * Pin getmeili/docs-scraper image to v0.12.8
    
    * Remove sitemap gen change
    
    * Tweak sitemap URL gen
    
    * url scheme ;)
    
    * + trailing slash ;)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,10 @@
 const RAILWAY_STATIC_URL = process.env.RAILWAY_STATIC_URL;
 module.exports = {
-  siteUrl: RAILWAY_STATIC_URL
-    ? `https://${RAILWAY_STATIC_URL}`
-    : "https://docs.railway.app/",
+  siteUrl:
+    // If we're on the prod environment, remap to the linked domain,
+    // otherwise use the provided static URL.
+    RAILWAY_STATIC_URL === "railway-docs-production.up.railway.app"
+      ? "https://docs.railway.app/"
+      : `https://${RAILWAY_STATIC_URL}/`,
   generateRobotsTxt: true,
 };
