c85698e8e433a2abfc38a3c6d23ca7df44cb4ed3
Ray Chen <ray@raychen.io>
2023-04-19 16:39:34 +0800

    Tweak sitemap URL gen

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,10 @@
 const RAILWAY_STATIC_URL = process.env.RAILWAY_STATIC_URL;
 module.exports = {
-  siteUrl: "https://docs.railway.app/",
+  siteUrl:
+    // If we're on the prod environment, remap to the linked domain,
+    // otherwise use the provided static URL.
+    RAILWAY_STATIC_URL === "railway-docs-production.up.railway.app"
+      ? "https://docs.railway.app/"
+      : RAILWAY_STATIC_URL,
   generateRobotsTxt: true,
 };
