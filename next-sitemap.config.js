aced480ab5fe08cf11ddd79529e5a7568ea0f38e
Ray Chen <ray@raychen.io>
2023-04-19 16:31:57 +0800

    Remove sitemap gen change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,5 @@
 const RAILWAY_STATIC_URL = process.env.RAILWAY_STATIC_URL;
 module.exports = {
-  siteUrl: RAILWAY_STATIC_URL
-    ? `https://${RAILWAY_STATIC_URL}`
-    : "https://docs.railway.app/",
+  siteUrl: "https://docs.railway.app/",
   generateRobotsTxt: true,
 };
