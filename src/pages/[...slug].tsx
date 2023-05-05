22192608ba0ec9a4b877728526d03cb97b18cd8d
samridht23 <samridht23@gmail.com>
2023-05-05 23:40:51 +0530

    hydration error fixed

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -6,103 +6,109 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { default as NextImage, ImageProps } from "next/legacy/image";
 import Link from "next/link";
-import { IconProps, Link as FeatherLinkIcon } from "react-feather";
+import { Link as FeatherLinkIcon } from "react-feather";
 import styled from "styled-components";
 
-const StyledHeading = styled.h2`
+const StyledLinkIcon = styled.a`
+  text-decoration: none;
+  position: absolute;
+  width:3rem;
+  height:2rem;
+  display:none;
+  align-items:center;
+  left: -2rem;
+  &:hover {
+      text-decoration:underline;
+  }
+`;
+
+const StyledLinkHeading = styled.a`
+  text-decoration: none;
+  position: absolute;
+  font-weight:bold;
+  &:hover {
+      text-decoration:underline;
+  }
+`;
+
+const StyledHeadingH2 = styled.h2`
   display: flex;
   align-items: center;
-`;
-
-const StyledLink = styled.a`
   position: relative;
-  text-decoration: none;
-  font-weight: bold;
+  padding: 1.5rem 0 0;
   &:hover{
-    text-decoration: underline;
-  }
-  svg {
-    position: absolute;
-    left: -1.7rem;
-    top: 20%;
-    visibility: hidden;
-    transition: visibility; 
-  }
-    @media (max-width: 767px) {
-    & > svg {
-      display: none;
+      ${StyledLinkIcon}{
+        display:flex;
+      }
+      @media (max-width: 1300px) {
+       ${StyledLinkIcon}{
+        display: none;
     }
   }
-  &:hover svg {
-    visibility: visible;
-  }
 `;
 
-
 const Image = (props: ImageProps) => (
-  <a
-    tw="block xl:-mx-8"
-    href={props.src as string}
-    target="_blank"
-    rel="noopener"
-  >
-    <NextImage {...props} />
-  </a>
+    <a
+        tw="block xl:-mx-8"
+        href={props.src as string}
+        target="_blank"
+        rel="noopener"
+    >
+        <NextImage {...props} />
+    </a>
 );
 
-const Heading = (props: IconProps) => {
-  const { children, id } = props;
-  return (
-    <StyledHeading id={id} tw="flex items-center space-x-2">
-      <StyledLink href={`#${id}`}>
-        <FeatherLinkIcon size={20}/>
-        {children}
-      </StyledLink>
-    </StyledHeading>
-  );
-};
 
 const components: Record<string, React.ElementType> = {
-  pre: CodeBlock,
-  h2: Heading,
-  Image,
-  Banner,
-  Link,
-  PriorityBoardingBanner,
+    pre: CodeBlock,
+    Image,
+    Banner,
+    Link,
+    PriorityBoardingBanner,
+    h2: ({id,children}) => (
+        <StyledHeadingH2 id={id}>
+            <StyledLinkIcon href={`#${id}`}>
+                <FeatherLinkIcon className="icon" size={20} />
+            </StyledLinkIcon>
+            <StyledLinkHeading href={`#${id}`}>
+                {children[1]}
+            </StyledLinkHeading>
+        </StyledHeadingH2>
+    ),
 };
 
 export default function PostPage({ page }: { page: Page }) {
-  const MDXContent = useMDXComponent(page.body.code);
+    const MDXContent = useMDXComponent(page.body.code);
 
-  return (
-    <Layout
-      frontMatter={{
-        title: page.title,
-      }}
-    >
-      <MDXContent components={components} />
-    </Layout>
-  );
+    return (
+        <Layout
+            frontMatter={{
+                title: page.title,
+            }}
+        >
+            <MDXContent components={components} />
+        </Layout>
+    );
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const page = allPages.find(
-    page =>
-      page._raw.flattenedPath ===
-      (params?.slug as string[] | undefined)?.join("/"),
-  );
+    const page = allPages.find(
+        page =>
+            page._raw.flattenedPath ===
+            (params?.slug as string[] | undefined)?.join("/"),
+    );
 
-  return {
-    props: {
-      page,
-    },
-  };
+    return {
+        props: {
+            page,
+        },
+    };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = allPages.map(page => page.url);
-  return {
-    paths,
-    fallback: false,
-  };
+    const paths = allPages.map(page => page.url);
+    return {
+        paths,
+        fallback: false,
+    };
 };
