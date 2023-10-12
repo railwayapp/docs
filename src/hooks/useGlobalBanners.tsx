2306e9240cedad7444741a151444eeadd45ac7a0
David <dbanys@users.noreply.github.com>
2023-10-12 11:49:06 -0700

    hide lw banner (#361)

--- src/hooks/useGlobalBanners.tsx
+++ src/hooks/useGlobalBanners.tsx
@@ -10,28 +10,28 @@ export interface GlobalBanner {
   variant?: BannerVariant;
 }
 
-const LaunchWeekBanner = ({
-  linkText,
-  isFirstStop,
-  isLastStop,
-}: {
-  linkText: string;
-  isFirstStop?: boolean;
-  isLastStop?: boolean;
-}) => (
-  <>
-    <span role="img">🚅</span>{" "}
-    <span>
-      <strong>Launch Week 01</strong> is now{" "}
-      {isFirstStop ? "boarding" : "in service"}.{" "}
-      {isFirstStop ? "First" : isLastStop ? "Last" : "Next"} stop:{" "}
-      <Link href="https://railway.app/launch-week-01" tw="underline">
-        {linkText}
-      </Link>
-      !
-    </span>
-  </>
-);
+// const LaunchWeekBanner = ({
+//   linkText,
+//   isFirstStop,
+//   isLastStop,
+// }: {
+//   linkText: string;
+//   isFirstStop?: boolean;
+//   isLastStop?: boolean;
+// }) => (
+//   <>
+//     <span role="img">🚅</span>{" "}
+//     <span>
+//       <strong>Launch Week 01</strong> is now{" "}
+//       {isFirstStop ? "boarding" : "in service"}.{" "}
+//       {isFirstStop ? "First" : isLastStop ? "Last" : "Next"} stop:{" "}
+//       <Link href="https://railway.app/launch-week-01" tw="underline">
+//         {linkText}
+//       </Link>
+//       !
+//     </span>
+//   </>
+// );
 
 const GLOBAL_BANNERS_KEY = "@railway/globalBanners";
 const useGlobalBannersLocalStorage = () =>
@@ -42,29 +42,9 @@ const useGlobalBannersLocalStorage = () =>
 export const allGlobalBanners: GlobalBanner[] = [
   // Day 1
   // {
-  //   id: "launch-week-day-1",
-  //   message: <LaunchWeekBanner linkText="Regions" isFirstStop />,
+  //   id: "some-id",
+  //   message: <LaunchWeekBanner linkText="Feature" isFirstStop />,
   // },
-  // Day 2
-  // {
-  //   id: "launch-week-day-2",
-  //   message: <LaunchWeekBanner linkText="Next-Gen Databases" />,
-  // },
-  // Day 3
-  // {
-  //   id: "launch-week-day-3",
-  //   message: <LaunchWeekBanner linkText="Horizontal Scaling" />,
-  // },
-  // Day 4
-  // {
-  //   id: "launch-week-day-4",
-  //   message: <LaunchWeekBanner linkText="Changesets" />,
-  // },
-  // Day 5
-  {
-    id: "launch-week-day-5",
-    message: <LaunchWeekBanner linkText="Workflows" isLastStop />,
-  },
 ];
 
 export const useGlobalBanners = () => {
