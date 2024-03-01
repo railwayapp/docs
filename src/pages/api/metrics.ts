e4f00f266a8a17bd7f6e452e0cb9356da7e860c1
Melissa Hale <melissakayh@gmail.com>
2024-03-01 15:39:46 -0600

    replica id labe

--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -3,11 +3,16 @@ import client from "prom-client";
 
 const register = new client.Registry();
 
+const replicaId = process.env.RAILWAY_REPLICA_ID || 'unknown';
+
 register.setDefaultLabels({
   app: "railway-docs",
+  replica_id: replicaId,
 });
 
 client.collectDefaultMetrics({ register });
+
+console.log(client.collectDefaultMetrics.metricsList)
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
     res.setHeader('Content-type', register.contentType);
