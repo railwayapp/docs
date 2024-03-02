847528f62c6f65ce74fbe70618aed338450652f3
Melissa Hale <melissakayh@gmail.com>
2024-03-01 18:20:44 -0600

    bump next and add corewebvitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,14 @@ import { useFathom } from "../hooks/useFathom";
 import { Page } from "../layouts/Page";
 import "../styles/fonts.css";
 import { ThemeProvider } from "../styles/theme";
+import { useReportWebVitals } from 'next/web-vitals'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useFathom(process.env.NEXT_PUBLIC_FATHOM_CODE ?? "", "docs.railway.app");
+  
+  useReportWebVitals((metric) => {
+    console.log(metric);
+  });
 
   return (
     <ThemeProvider>
