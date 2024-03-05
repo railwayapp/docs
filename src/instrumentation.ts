f97a64fdf80ac313ff607cc77787f83825f09899
Melissa Hale <melissakayh@gmail.com>
2024-03-05 17:38:01 -0600

    cleanup local dev stuff

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,10 +1,7 @@
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
   registerOTel({
     serviceName: "railway-docs",
-    spanProcessors: [new SimpleSpanProcessor(new OTLPTraceExporter())],
   });
 }
