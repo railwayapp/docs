f8e6c76cd1ae794c595589d54057a2e0db287012
Brody Over <10548119+brody192@users.noreply.github.com>
2025-02-07 18:46:48 -0500

    Fix title and link for the new page (#673)
    * Update github-pr-environment-actions.md
    
    * Update sidebar.ts
    
    * Update sidebar.ts

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -201,7 +201,7 @@ export const sidebarContent: ISidebarContent = [
             url: "https://blog.railway.com/p/gitlab-ci-cd",
           },
           makePage("GitHub Post-Deploy Actions", "tutorials"),
-          makePage("GitHub PR environment action", "tutorials")
+          makePage("GitHub PR Environment Actions", "tutorials")
         ],
       },
     ],
