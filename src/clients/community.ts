6fa3ab610a3594431fe3e640028e2d51f941f0b6
Brody Over <10548119+brody192@users.noreply.github.com>
2025-05-12 19:04:20 -0400

    remove forums code

--- src/clients/community.ts
+++
@@ -1,5 +0,0 @@
-import { COMMUNITY_API_URL } from "@/config";
-import { getSdk } from "../graphql/community/sdk";
-import { GraphQLClient } from "graphql-request";
-
-export const communityClient = getSdk(new GraphQLClient(COMMUNITY_API_URL));
