0f5bb92a0989a07b21135b9391edd356849a5963
Yan <61414485+yanthomasdev@users.noreply.github.com>
2026-01-09 16:29:01 -0300

    Fix Search modal contrast

--- src/components/Search/OpenModalButton.tsx
+++ src/components/Search/OpenModalButton.tsx
@@ -10,8 +10,8 @@ const OpenModalButton: React.FC = () => {
         onClick={() => searchStore.set(true)}
         css={[
           tw`flex items-center justify-between space-x-4 w-full`,
-          tw`rounded border border-gray-200 cursor-pointer`,
-          tw`px-2 py-2 md:py-1 text-gray-300 text-left`,
+          tw`rounded border border-gray-300 cursor-pointer`,
+          tw`px-2 py-2 md:py-1 text-gray-600 text-left`,
           tw`md:hover:border-pink-300`,
         ]}
       >
@@ -19,7 +19,7 @@ const OpenModalButton: React.FC = () => {
           <SearchIcon tw="w-4 h-4" />
           <span tw="text-sm">Search</span>
         </div>
-        <div tw="text-gray-300 text-sm hidden md:block">⌘K</div>
+        <div tw="text-gray-600 text-sm hidden md:block">⌘K</div>
       </button>
     </>
   );
