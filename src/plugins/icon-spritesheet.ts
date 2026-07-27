741698f5b40ae8b19c16904acb67676ed9d06b37
Mykal Machon <mykal@railway.com>
2026-07-27 10:06:27 -0700

    Preserve GitHub icon capitalization

--- src/plugins/icon-spritesheet.ts
+++ src/plugins/icon-spritesheet.ts
@@ -17,7 +17,11 @@ const colors = {
 const transformIconName = (fileName: string): string =>
   fileName
     .split("-")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .map(word =>
+      word === "github"
+        ? "GitHub"
+        : word.charAt(0).toUpperCase() + word.slice(1),
+    )
     .join("");
 
 const generateIcons = async () => {
